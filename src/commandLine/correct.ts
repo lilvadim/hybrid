20ec2f868f56ff095b9cddc4efd815d06a90ecd3
lilvadim <vadimmos2002@gmail.com>
2024-06-07 01:36:37 +0700

    fix

--- src/commandLine/correct.ts
+++ src/commandLine/correct.ts
@@ -2,31 +2,36 @@ import { ICommand } from "./command";
 import { ICommandSyntax } from "./syntax/commandSyntax";
 import { ICommandSemantic } from "./semantic/commandSemantic";
 import { addOptionsToCommand, removeOptionsFromCommand } from "./util/options";
-import Logger from "electron-log";
 
 export function correctInline(command: ICommand, semantic: ICommandSemantic, syntax: ICommandSyntax) {
 
     for (let i = 0; i < command.options.length; i++) {
         const option = command.options[i]
 
-        Logger.debug(JSON.stringify(option.option.option))
-
-        const optionSyntax = syntax.options.find(it => it.optionSynonyms.includes(option.option.option))
-        if (!optionSyntax) {
-            continue
+        const optionsToProcess = []
+        if (option.option.type === 'UNIX' && option.option.prefix && option.option.words) {
+            optionsToProcess.push(...option.option.words.map(w => option.option.prefix + w))
+        } else {
+            optionsToProcess.push(option.option.option)
         }
 
-        if (!optionSyntax.hasValue && !syntax.subcommands.includes(option.value || '')) {
-            option.value = undefined
-        }
-
-        const optionSemantic = semantic.options.filter(it => optionSyntax.optionSynonyms.includes(it.option))[0]
-        if (!optionSemantic) {
-            continue
-        }
-
-        Logger.debug(JSON.stringify(optionSemantic))
-        addOptionsToCommand(command, optionSemantic.whenAdded.add)       
-        removeOptionsFromCommand(command, optionSemantic.whenAdded.remove)
+        optionsToProcess.forEach(optionText => {
+            const optionSyntax = syntax.options.find(it => it.optionSynonyms.includes(optionText))
+            if (!optionSyntax) {
+                return
+            }
+    
+            if (!optionSyntax.hasValue && !syntax.subcommands.includes(option.value || '')) {
+                option.value = undefined
+            }
+    
+            const optionSemantic = semantic.options.filter(it => optionSyntax.optionSynonyms.includes(it.option))[0]
+            if (!optionSemantic) {
+                return
+            }
+    
+            addOptionsToCommand(command, optionSemantic.whenAdded.add)       
+            removeOptionsFromCommand(command, optionSemantic.whenAdded.remove)
+        })
     }
 }
